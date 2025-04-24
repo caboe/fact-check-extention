6ef36b438206fcadd6cfeb48de649f98f8d4f6af
caboe <ca_boe@yahoo.de>
2025-04-24 09:01:33 +0200

    add person to role

--- extension/src/lib/util/getSystemRole.svelte.ts
+++ extension/src/lib/util/getSystemRole.svelte.ts
@@ -2,9 +2,9 @@ import role from './role.svelte'
 export default function getSystemRole(person: string | null, range: number): string {
 	let systemRole = role.replace('{range}', String(range))
 
-	if (person) {
-		systemRole += `\n\nRespond in the style of ${person}.`
-	}
+	// Replace <person> tag with the provided person's name or an empty string
+	// TODO default  person
+	systemRole = systemRole.replace(/<person>/g, person || '')
 
 	return systemRole
 }
